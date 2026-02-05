063a97f2fcb857838642df4c6e40589c2793bba6
Max Lair <max@lairfamily.com>
2026-02-05 07:31:22 -0500

    Add pages/ skeleton (index, _app, welcome)

---
+++ pages/_app.tsx
@@ -0,0 +1,7 @@
+import '../app/global.css';
+import type { AppProps } from 'next/app';
+import * as React from 'react';
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+export default function HomePage() {
+  return (
+    <main style={{padding: 24}}>
+      <h1>Home (Pages Router)</h1>
+      <p>Move files here from the App Router when you're ready.</p>
+    </main>
+  );
+}

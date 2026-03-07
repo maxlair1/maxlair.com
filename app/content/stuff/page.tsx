4b881a812cff6d0adae3e0c378c0bca87d665711
Max Lair <max@lairfamily.com>
2026-03-07 09:51:29 -0500

    feat: enhance Stuff page layout and styling, add Badge component for image titles, and improve scrollbar design

--- app/content/stuff/page.tsx
+++ app/content/stuff/page.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import useContent from '../useContent';
 import styles from './page.module.css';
 import Gallery from '@root/components/bespoke/Gallery';
+import Badge from '@root/components/Badge';
 
 export default function Stuff(): React.JSX.Element {
     const { images } = useContent();
@@ -18,9 +19,12 @@ export default function Stuff(): React.JSX.Element {
                 {images.map((img, idx) => (
                     <div key={idx} className={styles.item}>
                         <div className={styles.imageWrapper}>
-                            <Image src={img.url} fill style={{objectFit: 'contain'}} alt={img.title}/>
+                            <Image src={img.url} fill style={{objectFit: 'contain'}} alt={img.title} preload/>
+                            {/* <div className={styles.actions}>
+                                <Badge>{img.title}</Badge>
+                            </div> */}
                         </div>
-                        <figcaption>caption test</figcaption>
+                        <Badge>{img.title}</Badge>
                     </div>
                 ))}
             </div>
